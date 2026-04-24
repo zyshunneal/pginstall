59a25313729928b99e788eabc7fae3dea5dd4197
zhaoyueshun <zhaoyueshun@zyshunmac-84.local>
2026-04-24 13:18:35 +0800

    提交当前初始化配置调整

--- script/user_init.sh
+++ script/user_init.sh
@@ -3,22 +3,21 @@
 # Feedback and improvements are welcome.
 # date: 2019-10-02 18:29:39
 
-set -euo pipefail
 
-SCRIPT=$(readlink -f "$0")
-cd "$(dirname "$SCRIPT")"
+#User initialization script
+SCRIPT=$(readlink -f $0)
+cd $(dirname $SCRIPT)
 
 
 echo_success(){
     cat <<EOF
     {
         "result": 0,
-        "info": success,
+        "info": $1,
         "data": {}
     }
 EOF
 }
-
 
 echo_failure(){
     local rc=-1
@@ -33,34 +32,20 @@ echo_failure(){
 EOF
 }
 
-
 echo_log(){
-    local timestamp
-    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    timestamp=`date "+%Y-%m-%d %H:%M:%S"`
     echo "${timestamp} $*" >&2
 }
 
 
+
 help () {
-    cat << EOF
+        cat << EOF
 Usage: $0 [OPTIONS]
 Options:
     --servername -S The name of the business assumed by the database user
 EOF
-}
 
-
-require_env() {
-    local name="$1"
-    if [ -z "${!name:-}" ]; then
-        echo_failure "parameter parse error. Please set ${name}" ""
-        exit 1
-    fi
-}
-
-
-sql_escape() {
-    printf "%s" "$1" | sed "s/'/''/g"
 }
 
 
@@ -75,48 +60,32 @@ do case $1 in
             exit 0
             ;;
         *)
-            echo_failure "Parameter parse error. Invalid argument: $1" ""
-            exit 1
+            echo_failure "Parameter parse error. Invalid argument: $1"
+            exit -1
             ;;
     esac
     shift
 done
 
-if [ -z "${servername:-}" ];then
-    echo_failure "parameter parse error. Please select servername" ""
-    exit 1
-fi
-
-require_env PGINSTALL_DBA_PASSWORD
-require_env PGINSTALL_MONITOR_PASSWORD
-require_env PGINSTALL_STATS_PASSWORD
-require_env PGINSTALL_REPLICATION_PASSWORD
-require_env PGINSTALL_BUSINESS_USER_PASSWORD
+if [ -z  "${servername}" ];then
+    echo_failure "parameter parse error. Please select servername"
+    exit -1
+fi  
 
 function user_initialization()
-{
-    local vailduntil
-    local sername
-    local name
-    local username
-    local dba_password
-    local monitor_password
-    local stats_password
-    local replication_password
-    local business_password
-
-    vailduntil=$(date -d +90day +'%Y-%m-%d %H:%M:%S')
+{  
+    vailduntil=`date -d +90day +'%Y-%m-%d %H:%M:%S'`
+    passwd=`pwgen -cnCy  16 -1`
+    p1=${passwd//:/?}
+    p2=${p1//@/?}
+    p3=${p2//!/?}
+    p4=${p3//\'/?}
+    p5=${p4//\"/?}
+    password=${p5}
     sername=$1
     name=${sername//-/}
-    username="dbuser_${name}"
-
-    dba_password=$(sql_escape "${PGINSTALL_DBA_PASSWORD}")
-    monitor_password=$(sql_escape "${PGINSTALL_MONITOR_PASSWORD}")
-    stats_password=$(sql_escape "${PGINSTALL_STATS_PASSWORD}")
-    replication_password=$(sql_escape "${PGINSTALL_REPLICATION_PASSWORD}")
-    business_password=$(sql_escape "${PGINSTALL_BUSINESS_USER_PASSWORD}")
-
-    psql -v ON_ERROR_STOP=1 -U postgres << EOF
+    echo "username:dbuser_${name}  password:${password}" > /home/postgres/.userinfo.conf
+    psql -U postgres  << EOF 
     CREATE ROLE dbrole_offline;
     ALTER ROLE dbrole_offline WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
     CREATE ROLE dbrole_readonly;
@@ -128,13 +97,13 @@ function user_initialization()
     CREATE ROLE dbrole_sa;
     ALTER ROLE dbrole_sa WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
     CREATE ROLE dbuser_dba;
-    ALTER ROLE dbuser_dba WITH SUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD '${dba_password}' VALID UNTIL '${vailduntil}';
+    ALTER ROLE dbuser_dba WITH SUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md57bb81c95d81079e81981f7c8cf84d586' VALID UNTIL       '${vailduntil}';
     CREATE ROLE dbuser_monitor;
-    ALTER ROLE dbuser_monitor WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD '${monitor_password}' VALID UNTIL '${vailduntil}';
+    ALTER ROLE dbuser_monitor WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md569ef1c948b85b237f264a58d3cb0730b' VALID UNTIL '${vailduntil}';
     CREATE ROLE dbuser_stats;
-    ALTER ROLE dbuser_stats WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD '${stats_password}' VALID UNTIL '${vailduntil}';
+    ALTER ROLE dbuser_stats WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5425da66bfc2f7d5df6905c0492b0033c' VALID UNTIL   '${vailduntil}';
     CREATE ROLE replication;
-    ALTER ROLE replication WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN REPLICATION NOBYPASSRLS PASSWORD '${replication_password}';
+    ALTER ROLE replication WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN REPLICATION NOBYPASSRLS PASSWORD 'md579d14c0f4deee5ecaf50bb5f20b0aabd';
     GRANT dbrole_offline TO dbuser_stats GRANTED BY postgres;
     GRANT dbrole_readonly TO dbrole_offline GRANTED BY postgres;
     GRANT dbrole_readwrite TO dbrole_readwrite_with_delete GRANTED BY postgres;
@@ -143,7 +112,7 @@ function user_initialization()
     create database "putong-${sername}";
 EOF
 
-    psql -v ON_ERROR_STOP=1 -U postgres -d "putong-${sername}" << EOF
+    psql -U postgres  -d "putong-${sername}" << EOF
     create schema yay;
     CREATE TABLE yay.init_result_check_table
     (
@@ -168,13 +137,13 @@ EOF
     ALTER DEFAULT PRIVILEGES IN SCHEMA yay GRANT ALL ON functions TO dbrole_readonly;
     ALTER DEFAULT PRIVILEGES IN SCHEMA yay GRANT ALL ON functions TO dbrole_readwrite;
     GRANT CONNECT ON DATABASE "putong-${sername}" TO GROUP dbrole_readonly,dbrole_readwrite;
-    create user ${username} ENCRYPTED PASSWORD '${business_password}' in group dbrole_readwrite_with_delete VALID UNTIL '${vailduntil}';
-    GRANT dbrole_readwrite_with_delete TO ${username} GRANTED BY postgres;
+    create user dbuser_${name} ENCRYPTED PASSWORD '${password}' in group dbrole_readwrite_with_delete VALID UNTIL   '${vailduntil}';
+    GRANT dbrole_readwrite_with_delete TO dbuser_${name} GRANTED BY postgres;
     GRANT USAGE ON SCHEMA yay TO "dbuser_monitor";
-EOF
 
-    echo_success
+EOF
+    echo_success "{'username':dbuser_${name}, 'password':${password}}"
 }
 
 
-user_initialization "${servername}"
+user_initialization ${servername}
